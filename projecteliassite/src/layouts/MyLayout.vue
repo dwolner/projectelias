44063f51fb28a5a0f696511e60ad4cccdc15a099
Dan Wolner <daniel.wolner@gmail.com>
2020-03-01 23:17:32 -0800

    scrollto fix for switching pages first

--- projecteliassite/src/layouts/MyLayout.vue
+++ projecteliassite/src/layouts/MyLayout.vue
@@ -146,16 +146,28 @@ export default {
         nav(item) {
             console.log('nav: ', item)
             if (item.sectionID) {
-                if (this.$router.currentRoute.path !== '/') this.$router.push('/')
+                if (this.$router.currentRoute.path !== '/') {
+                    this.$router.push('/')
 
-                if (item.sectionID === 'Homebot') {
-                    this.$root.$emit('goToHomebot', true)
-                    var el = document.getElementById('BuyersSellers')
-                    // var element = document.getElementById('BuyersSellersButtons')
-                    setTimeout(() => { this.scrollToElement('BuyersSellersButtons', el.offsetTop) }, 1000)
+                    if (item.sectionID === 'Homebot') {
+                        this.$root.$emit('goToHomebot', true)
+                        var el = document.getElementById('BuyersSellers')
+                        // var element = document.getElementById('BuyersSellersButtons')
+                        setTimeout(() => { this.scrollToElement('BuyersSellersButtons', el.offsetTop) }, 1000)
+                    } else {
+                        // var element = document.getElementById(item.sectionID)
+                        setTimeout(() => { this.scrollToElement(item.sectionID, 75) }, 1000)
+                    }
                 } else {
-                    // var element = document.getElementById(item.sectionID)
-                    setTimeout(() => { this.scrollToElement(item.sectionID, 75) }, 1000)
+                    if (item.sectionID === 'Homebot') {
+                        this.$root.$emit('goToHomebot', true)
+                        var el = document.getElementById('BuyersSellers')
+                        // var element = document.getElementById('BuyersSellersButtons')
+                        this.scrollToElement('BuyersSellersButtons', el.offsetTop)
+                    } else {
+                        // var element = document.getElementById(item.sectionID)
+                        this.scrollToElement(item.sectionID, 75)
+                    }
                 }
             }
             if (item.route) this.$router.push(item.route)
