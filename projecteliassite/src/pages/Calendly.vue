4e017221081d92054195337e607e32ce83066150
Dan Wolner <daniel.wolner@gmail.com>
2020-04-14 19:41:32 -0700

    added calendly page and link to contact section

---
+++ projecteliassite/src/pages/Calendly.vue
@@ -0,0 +1,40 @@
+<template>
+    <q-page class="flex flex-center">
+        
+        <div class="calendly-inline-widget" data-url="https://calendly.com/richardeliasteam" style="min-width:320px;height:630px;"></div>
+
+    </q-page>
+</template>
+
+<script>
+
+export default {
+    name: 'Calendly',
+
+    components: {
+    },
+
+    data() {
+        return {
+        }
+    },
+
+    computed: {
+       
+    },
+
+    methods: {
+
+    },
+
+    created() {
+        let recaptchaScript = document.createElement('script')
+            recaptchaScript.setAttribute('src', 'https://assets.calendly.com/assets/external/widget.js')
+        document.head.appendChild(recaptchaScript)
+    }
+}
+</script>
+
+<style scoped>
+   
+</style>
