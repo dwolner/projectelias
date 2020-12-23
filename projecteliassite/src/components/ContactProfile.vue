d42a6fb1a5494f64a8c819691cd67c0c617f0b84
Dan Wolner <daniel.wolner@gmail.com>
2020-12-23 17:47:20 -0500

    christopher fixes, testing agent param

--- projecteliassite/src/components/ContactProfile.vue
+++ projecteliassite/src/components/ContactProfile.vue
@@ -1,11 +1,11 @@
 <template>
     <div id="contact-profile" class="row">
         <div class="col-xs-12 col-sm-6 col-md-4 q-pa-sm" :align="$q.screen.width > 599 ? 'right' : 'center'">
-            <q-img :src="`statics/team/${ agentData.imageFilename }`" style="max-width: 300px;" />
+            <q-img :src="`statics/team/${ agentData.avatar }`" style="max-width: 300px;" />
         </div>
         <div class="col-xs-12 col-sm-6 col-md-8 q-pa-sm" :align="$q.screen.width > 599 ? 'left' : 'center'">
             <div>
-                <p class="q-mb-sm" style="font-size: 1rem;"><strong>{{ agentData.name }}</strong> | {{ agentData.title }}</p>
+                <p class="q-mb-sm" style="font-size: 1rem;"><strong>{{ agentData.name }}</strong> | {{ agentData.role }}</p>
                 <p class="q-my-sm" style="font-size: 1rem;">DRE# {{ agentData.dreNumber }}</p>
                 <p class="q-my-sm" style="font-size: 1rem;">{{ agentData.email }}</p>
                 <p class="q-my-sm" style="font-size: 1rem;">✆: {{ agentData.phone }}</p>
